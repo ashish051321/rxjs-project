61b4c2d0147ca8ed16a2155facae729dc67b9ad7
ashish051321 <ashish.25jl@gmail.com>
2017-08-21 00:53:55 +0530

    made changes to main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,47 @@
-alert("Hello bandhu !, I have arrived !");  
+// alert("Hello bandhu !, I have arrived !");  
+
+import {Observable} from 'rxjs';
+
+let numbers= [1,2,3,4,5,6,7]
+let source = Observable.from(numbers);
+//Here I am hiding the array behind the Observable and produce a stream of data.
+
+
+/*
+
+When I want to listen to that stream of data, that's when I subscribe to my Observable.
+I need to pass in an Observer in the subscribe method.
+The Observer, we are going to create very formally int the form of a class
+that has to have three methods:-
+
+next() - This is the method by Observable will invoke on my Observer when there is a value to produce.
+
+error() - called when the Observable object produces an error.
+
+complete() - If the observable knows that it has exhausted the data source, it has produced 
+all the values it can, t will invoke this complete method.
+
+
+*/
+
+class MyObserver{
+
+    next(value){
+        console.log(value); 
+    }
+
+    error(e)
+    {
+        console.log(`error ${e}`);
+    }
+
+    complete(){
+        console.log(":we are complete");
+    }
+}
+
+//Once I have my Obsesrver in place, lets instantiate it an pass it to the subscribe method.
+
+source.subscribe(new MyObserver());
+
+
